ad508167fdd97314149e1d82eabcb90eaa065360
lijing123451 <343301795@qq.com>
2022-11-27 12:03:24 +0800

    这是第七周的作业

--- ch6/js/index.js
+++ ch6/js/index.js
@@ -96,4 +96,43 @@ $(function () {
             scrollTop: 0
         }, 300)
     })
+
+    /* 二维码滑出效果 */
+    $('.qr-code .ticket').hover(function () {
+        /* 让二维码滑出来 */
+        $('.qr-code div').stop(true).animate({/* stop停止当前动画 */
+            left:'-100px'
+        })
+    },function () {
+        /* 让二维码收出来 */
+        $('.qr-code div').stop(true).animate({
+            left:'0px'
+        })
+    })
+
+    /* 顶部搜索框交互 */
+    $(document).scroll(function () {
+        /* 获取到顶部的距离 */
+        var topDistance = $('html,body').scrollTop();
+        /* 判断 */
+        if(topDistance > 500){
+            /* 如果滚动距离大于500就滑下来 */
+            $('.top-search-box').slideDown(300)
+        }else{
+            /* 如果滚动距离小于500就收上去 */
+            $('.top-search-box').slideup(300)
+        }
+    })
+
+    /* 楼梯跳转 */
+    $('.floor li').click(function(){
+        /* 获取索引 */
+        var index = $(this).index();
+        /* 选中每一个板块到顶部的偏移 */
+        var topOffset = $('.floorBox').eq(index).offset().top;
+        /* 让滚动条滚到这个位置 */
+        $('html,body').animate({
+            scrollTop: topOffset -50
+        })
+    })
 })
